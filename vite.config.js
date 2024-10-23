a9a23268aaf2ee3a3eadf4c5b81bd3df11463515
rashabaroudi <eng.rashaba95@gmail.com>
2024-10-24 00:06:59 +0300

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/Elevate-E-commerce-task/",
   plugins: [react()],
+  build:{
+    target: "esnext", // or "es2019",
+  },
 })
