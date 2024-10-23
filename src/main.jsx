1d53a4092b053660e75e1a04bd54a5de4d67b5bb
rashabaroudi <eng.rashaba95@gmail.com>
2024-10-24 00:27:06 +0300

    setup gh-pages again

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/Elevate-E-commerce-task'>
+  <BrowserRouter >
     <ProductContextProvider>
       <Navbar/>
       <App />
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Elevate-E-commerce-task/",
+  
   plugins: [react()],
   build:{
     target: "esnext", // or "es2019",
