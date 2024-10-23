03a3590ed5b16392ebd96b6b56d51052ffbd5e0c
rashabaroudi <eng.rashaba95@gmail.com>
2024-10-24 00:49:55 +0300

    setup gh-pages again

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter basename='/'>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <ProductContextProvider>
       <Navbar/>
       <App />
